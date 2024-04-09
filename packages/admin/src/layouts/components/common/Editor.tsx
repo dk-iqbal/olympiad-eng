c02672ae1016417829fa4e47e758b47a130e0acf
1257148iqbal <iqbalhossain1095@gmail.com>
2024-04-09 15:39:40 +0600

    removed ckeditor

--- packages/admin/src/layouts/components/common/Editor.tsx
+++ packages/admin/src/layouts/components/common/Editor.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { CKEditor } from '@ckeditor/ckeditor5-react'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import { Box } from '@mui/material'
 
 const Editor = (props: any) => {
@@ -18,7 +16,6 @@ const Editor = (props: any) => {
 
   return (
     <Box sx={{ width: { sm: '100%', xs: '100%' } }} style={{ margin: 'auto' }}>
-      <CKEditor editor={ClassicEditor} data={formData?.description} onChange={handleEditorDataChange} />
 
       {/* <div>
         <h3>Editor Content:</h3>
